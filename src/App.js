0401fef45af64fbf766294655de0b710a847ce2f
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-28 23:21:53 +0900

    ⚡️ making UI and make use of redux to get a data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import "./app.css";
+import { useSelector } from "react-redux";
+/*useSelectorはreduxのstateを取得するためのhookです*/
+
 function App() {
-  return <div className="App">main</div>;
+  const postlist = useSelector((state) => state.posts.value);
+
+  return <div className="App">
+    <div>
+      <h1>React-Redux掲示板</h1>
+    </div>
+    <div className="addPost">
+      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Content" />
+      <button>Add</button>
+      <hr />
+    </div>
+    <div className="displayPosts">
+      {postlist.map((post) => (
+         <div>
+          <h1>{post.name}</h1>
+         </div>
+      ))}
+     
+      </div>
+  </div>;
 }
 
 export default App;
