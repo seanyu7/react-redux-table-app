97988cc8fa75f7bee8f90aad934f3238a1cbc1fb
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-29 02:04:45 +0900

    ✨ made a delete button in use

--- src/App.js
+++ src/App.js
@@ -1,43 +1,61 @@
 import "./app.css";
 import { useSelector, useDispatch } from "react-redux";
 /*useSelectorはreduxのstateを取得するためのhookです*/
-import {addPost} from "./features/Post";
+import { addPost, deletePost } from "./features/Post";
+import { useState } from "react";
+
 function App() {
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+
   const postlist = useSelector((state) => state.posts.value);
 
   const dispatch = useDispatch();
-  
+
   const handleClick = () => {
-dispatch(addPost(
-  {
-    id:0,
-    name:
-  content:
-  }
-))
+    dispatch(
+      addPost({
+        id: postlist.length,
+        name: name,
+        content: content,
+      })
+    );
+    setContent("");
+    setName("");
   };
 
-  return <div className="App">
-    <div>
-      <h1>React-Redux掲示板</h1>
-    </div>
-    <div className="addPost">
-      <input type="text" placeholder="Name" />
-      <input type="text" placeholder="Content" />
-      <button onClick={() => handleClick()}>Add</button>
-      <hr />
-    </div>
-    <div className="displayPosts">
-      {postlist.map((post) => (
-         <div key={post.id} className="post">
-          <h1 className="postName">{post.name}</h1>
-          <h1 className="postContent">{post.content}</h1>
-          <button>delete</button>
-         </div>
-      ))}
-     
+  return (
+    <div className="App">
+      <div>
+        <h1>React-Redux掲示板</h1>
       </div>
-  </div>;
+      <div className="addPost">
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type="text"
+          placeholder="Content"
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
+        />
+        <button onClick={() => handleClick()}>Add</button>
+        <hr />
+      </div>
+      <div className="displayPosts">
+        {postlist.map((post) => (
+          <div key={post.id} className="post">
+            <h1 className="postName">{post.name}</h1>
+            <h1 className="postContent">{post.content}</h1>
+            <button onClick={()=> dispatch(deletePost({id:post.id}))}>delete</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/features/Post.js
+++ src/features/Post.js
@@ -1,17 +1,20 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { PostsData } from "./DummyData";
 
 export const postsSlice = createSlice({
-    name: "posts",
-    initialState: { value: PostsData},
-    reducers: {
-        addPost: (state, action) => {
-            state.value.push(action.payload);
-            /*payloadはデータのことを指している。データをpost dataという配列にPushしていくという意味*/
-            /*addPost is Action creater in the slice*/
-        },
+  name: "posts",
+  initialState: { value: PostsData },
+  reducers: {
+    addPost: (state, action) => {
+      state.value.push(action.payload);
+      /*payloadはデータのことを指している。データをpost dataという配列にPushしていくという意味*/
+      /*addPost is Action creater in the slice*/
     },
+    deletePost: (state, action) => {
+      state.value = state.value.filter((post) => post.id !== action.payload.id);
+    },
+  },
 });
 
-export const {addPost} = postsSlice.actions;
+export const { addPost, deletePost} = postsSlice.actions;
 export default postsSlice.reducer;
